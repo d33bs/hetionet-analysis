e404e2ef1ece09f5960a9154a8b55fc9ec67e08d
d33bs <dave.bunten@cuanschutz.edu>
2024-11-06 06:06:44 -0700

    linting

--- src/bioprocess_metapath_to_gene_pval_and_dwpc/gather_data.py
+++ src/bioprocess_metapath_to_gene_pval_and_dwpc/gather_data.py
@@ -14,10 +14,14 @@
 
 # # Biological Process and Gene Metapath Data Gathering
 #
-# - Each value from `BP.csv` is a source and each value from `Gene.csv` is a target. 
-# - Each source + target pairing may have a metapath which is found within `metapaths.csv`.
-# - For each pair metapath we need the DWPC and p-value stored in a table for reference.
-# - Ignore metapaths found within `metapaths_ignore.csv`.
+# - Each value from `BP.csv` is a source and each value
+# from `Gene.csv` is a target.
+# - Each source + target pairing may have a metapath which
+# is found within `metapaths.csv`.
+# - For each pair metapath we need the DWPC and p-value
+# stored in a table for reference.
+# - Ignore metapaths found within
+# `metapaths_ignore.csv`.
 
 # +
 import pathlib
@@ -27,7 +31,7 @@ from typing import Generator, Iterator, Tuple
 import lancedb
 import pandas as pd
 import pyarrow as pa
-import pyarrow.csv as csv
+from pyarrow import csv
 
 from hetionet_utils.database import HetionetNeo4j
 
